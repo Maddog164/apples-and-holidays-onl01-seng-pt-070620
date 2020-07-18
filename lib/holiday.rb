2f9d937c5d070f5ad2688324bd9e1cfc955aff04
Denise Henderson <denise@henderson3.us>
2020-07-18 13:59:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,9 +89,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_supplies.each do |season, holiday|
+    binding.pry
     puts season.to_s.capitalize + ":"
   end #do
-  binding.pry
+  #binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)

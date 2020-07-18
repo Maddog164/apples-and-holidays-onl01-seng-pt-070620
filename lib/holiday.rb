65d14f3ee58388a7e1533c391c1789ef863fbb24
Denise Henderson <denise@henderson3.us>
2020-07-18 13:50:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_winter_holiday_supplies(holiday_hash)
       end #do
     end #if
   end #do
-  return new_supplies
+  return new_supplies.flatten
 end #def
 
 def all_supplies_in_holidays(holiday_hash)

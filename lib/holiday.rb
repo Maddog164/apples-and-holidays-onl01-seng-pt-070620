d8af1e4cd8569cd207c89bbd30210edcaec22553
Denise Henderson <denise@henderson3.us>
2020-07-18 14:11:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,7 @@ def all_supplies_in_holidays(holiday_hash)
   #hold_season = ""
   holiday_supplies.each do |season, holiday|
     binding.pry
-    puts season.to_s.capitalize + ": "
+    puts season.to_s.capitalize + ":"
   end #do
   #binding.pry
   #puts hold_season
